7d16742087348ac19deb8c04379cfe0ac0454159
peter_alfie <alfie1104@gmail.com>
2020-07-15 08:59:59 +0900

    part 10

---
+++ graphql/db.js
@@ -0,0 +1,44 @@
+let movies = [
+  {
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1,
+  },
+  {
+    id: 1,
+    name: "Second",
+    score: 8,
+  },
+  {
+    id: 2,
+    name: "Third",
+    score: 99,
+  },
+];
+
+export const getMovies = () => movies;
+
+export const getById = (id) => {
+  const filteredMovies = movies.filter((movie) => id === movie.id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
